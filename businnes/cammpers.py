e335abd3b21a670d1a16cdb75815b2a9c11e822e
TitusRulos <jujotiga@gmail.com>
2024-01-11 12:17:12 -0500

    123123

--- businnes/cammpers.py
+++ businnes/cammpers.py
@@ -1,56 +1,81 @@
 import json
 import os
-
+from ..commons.utils import limpiar_pantalla
 
 def load_campers_json():
     try:
-      with open(os.path.join("data", "campers.json"), 'r') as archivo_json:        
-        lista_campers = json.load(archivo_json)
-        print("La lista de campers ha sido guardada")
-        return lista_campers
+        with open(os.path.join("data", "campers.json"), 'r') as archivo_json:        
+            lista_campers = json.load(archivo_json)
+            print("La lista de campers ha sido cargada")
+            return lista_campers
     except Exception as e:
-      print(f"Error al guardar el archivo: {e}")
+        print(f"Error al cargar el archivo: {e}")
+        return {'campers_inscritos': []}  # Retorna un diccionario con una lista vacía si hay algún error al cargar
 
-lista_campers = {}
+lista_campers = load_campers_json()
+
+def validar_edad(edad):
+    try:
+        edad = int(edad)
+        return 12 <= edad <= 70
+    except ValueError:
+        return False
+
+def validar_identificacion(identificacion):
+    return len(identificacion) < 10
 
 def crear_camper():
     nombre = input("Ingrese el nombre del cliente: ")
-    edad = (input("Ingrese la edad del cliente: "))
+
+    # Validar que el nombre sea un string
+    while not isinstance(nombre, str) or not nombre:
+        
+        print("Por favor, ingrese un nombre válido.")
+        nombre = input("Ingrese el nombre del cliente: ")
+
+    edad = input("Ingrese la edad del cliente: ")
+
+    # Validar que la edad sea un número entre 12 y 70
+    while not validar_edad(edad):
+        limpiar_pantalla()
+        print("Por favor, ingrese una edad válida (entre 12 y 70).")
+        edad = input("Ingrese la edad del cliente: ")
+
     email = input("Ingrese el correo electrónico del cliente: ")
     telefono = input("Ingrese el número de teléfono del cliente: ")
     direccion = input("Ingrese la dirección del cliente: ")
+    identificacion = input("Ingrese su número de cédula: ")
+
+    # Validar que la identificación tenga menos de 10 dígitos
+    while not validar_identificacion(identificacion):
+        print("Por favor, ingrese una identificación válida (menos de 10 dígitos).")
+        identificacion = input("Ingrese su número de cédula: ")
 
     camper = {
         'nombre': nombre,
         'edad': edad,
         'email': email,
         'telefono': telefono,
-        'direccion': direccion
+        'direccion': direccion,
+        'id': identificacion
     }
-    lista_campers.update(camper)
-    print(lista_campers)
-    print("Se creó el camper con éxito")
-    guardar_json()
 
+    if 'campers_inscritos' not in lista_campers:
+        lista_campers['campers_inscritos'] = []
 
+    lista_campers['campers_inscritos'].append(camper)
 
-
-def guardar_json():
     try:
-      with open(os.path.join("data", "campers.json"), 'w') as archivo_json:
-        json.dump(lista_campers, archivo_json, indent=2)
-        print("La lista de campers ha sido guardada")
-    except FileNotFoundError: 
-        print("El archivo no existe. Puede que aún no haya campers guardados.")
-    except json.JSONDecodeError:
-        print("Error al decodificar el archivo JSON . El formato podría ser incorrecto.")
+        with open(os.path.join("data", "campers.json"), 'w') as archivo_json:
+            json.dump(lista_campers, archivo_json, indent=2)
+            print("El camper ha sido guardado exitosamente")
     except Exception as e:
-        print("Error desconocido:")
-      
-
-      
+        print(f"Error al guardar el camper: {e}")
 
 def listar_campers():
     print("Listado de campers: ")
-    for camper in lista_campers:
-        print(camper)
+    if 'campers_inscritos' in lista_campers:
+        for camper in lista_campers['campers_inscritos']:
+            print(camper)
+    else:
+        print("No hay campers inscritos actualmente")
--- main.py
+++ main.py
@@ -7,24 +7,33 @@ from businnes.cammpers import crear_camper,listar_campers,load_campers_json,list
 
 # funtions
 def campers():
-    load_campers_json()
     limpiar_pantalla()
-    op=menu_campers()
-    if op==1:
-       crear_camper()
-       input("Clic cualquier teclas [continuar]: ")
+    op = menu_campers()
+
+    if op == 1:
+        while True:
+            crear_camper()
+            volver = input("¿Crear otro camper? [y/n]: ")
+            if volver.lower() != "y":
+                break
+            
     if op==2:
        listar_campers()
        input("Clic cualquier teclas [continuar]: ")
+
+
 def trainers():
     limpiar_pantalla()    
     op=menu_trainers()
+
 def matriculas():
     limpiar_pantalla()    
     op=menu_matriculas()
+
 def aulas():
     limpiar_pantalla()    
     op=menu_aulas()
+
 def reportes():
     limpiar_pantalla()    
     op=menu_reportes()
